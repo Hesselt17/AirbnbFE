a9cad5967beaaeb2bf0da6712f41befa8a55ef18
Tommy Hessel <thomas.hessel@duke.edu>
2019-05-28 21:55:43 +0100

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab4PageModule' },
+  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab4PageModule' },
+  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab4PageModule' },
+  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
+  { path: 'tab5', loadChildren: './tab5/tab5.module#Tab5PageModule' }
 ];
 @NgModule({
   imports: [
