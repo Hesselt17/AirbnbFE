a5aeff2e181d8038f5e714fa0ff2950152fbaeba
Tommy Hessel <thomas.hessel@duke.edu>
2019-05-28 22:37:51 +0100

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', loadChildren: './registration/registration.module#RegistrationPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'tab1', loadChildren: './tab1/tab1.module#Tab4PageModule' },
   { path: 'tab2', loadChildren: './tab2/tab2.module#Tab4PageModule' },
