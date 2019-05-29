d9d38b2c86fcd266179f1024ad5d5261d2acbefd
Tommy Hessel <thomas.hessel@duke.edu>
2019-05-29 09:13:08 +0100

    route fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ const routes: Routes = [
   { path: '', loadChildren: './registration/registration.module#RegistrationPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab4PageModule' },
-  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab4PageModule' },
-  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab4PageModule' },
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
+  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
   { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
   { path: 'tab5', loadChildren: './tab5/tab5.module#Tab5PageModule' },
   { path: 'details', loadChildren: './details/details.module#DetailsPageModule' }
