f53086790a4d5ebe18c0bcd0651782c97e947e6a
Tommy Hessel <thomas.hessel@duke.edu>
2019-05-30 03:13:26 +0100

    models 1.1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,18 +9,34 @@ import { listing } from '../models/listing.model';
 })
 export class Tab1Page {
 
-  public listings: Array<listing> = [];
+  public listing: Array<listing> = [];
 
   constructor() {
 
     let lst1 = new listing();
-    lst1.address = "1600 Pennsylvania Ave.";
+    lst1.address = "Ranch Style";
 
     let lst2 = new listing();
-    lst1.address = "Reunion Tower";
+    lst2.address = "Red Keep";
 
-    this.listings.push(lst1);
-    this.listings.push(lst2);
+    let lst3 = new listing();
+    lst3.address = "White Zigzag";
+
+    let lst4 = new listing();
+    lst4.address = "Sunset Castle";
+
+    let lst5 = new listing();
+    lst5.address = "Twilight Abode";
+
+    let lst6 = new listing();
+    lst6.address = "Suburban Retreat";
+
+    this.listing.push(lst1);
+    this.listing.push(lst2);
+    this.listing.push(lst3);
+    this.listing.push(lst4);
+    this.listing.push(lst5);
+    this.listing.push(lst6);
 
 
   }
